e6b3a2a3db7fa855d945ba8785e513e4d4c0fb05
BegaevAlexey <ph.begaev@gmail.com>
2018-01-09 11:45:46 +0300

    first commit - base project, not chenges

---
+++ cmake-build-debug/CMakeFiles/calibrateCameraCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibrateCameraCPP.dir/main.cpp.o"
+  "calibrateCameraCPP.pdb"
+  "calibrateCameraCPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibrateCameraCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
